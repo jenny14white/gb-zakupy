5a9ee09833f685b96af2ff496cb40cfdbfa6cc71
jenny14white <lilianaszymikowska@wp.pl>
2026-07-08 22:56:19 +0200

    Update PublicHeader.jsx

--- gb-zakupy/src/components/public/PublicHeader.jsx
+++ gb-zakupy/src/components/public/PublicHeader.jsx
@@ -9,20 +9,31 @@ export default function PublicHeader() {
 
       <div className="intro-box">
         <p>
-          Poniżej znajduje się formularz, w którym możesz zgłosić rzeczy
-          potrzebne do biura.
+          <strong>🛒 Brakuje czegoś w biurze?</strong> A może obawiasz się, że
+          później o tym zapomnisz? Wpisz poniżej swoje imię oraz to, czego
+          aktualnie potrzebujesz.
         </p>
 
         <p>
-          Mogą to być długopisy, flamastry, mleko do kuchni, cukier, kawa,
-          herbata albo inne produkty, które lubicie najbardziej lub zauważycie,
-          że się kończą a mnie nie będzie lub przeoczę.
+          ☕ Kończy się Twoja ulubiona herbata? 🥛 Brakuje mleka do kawy?
+          🖊️ Przydałyby się nowe długopisy lub przy ostatnim zamówieniu coś
+          zostało przeoczone? A może osoba odpowiedzialna za zamówienia jest
+          akurat nieobecna? To idealne miejsce, aby zostawić taką informację.
         </p>
 
         <p>
-          W sekcji <strong>„Aktualna lista zakupowa”</strong> znajdziesz
-          produkty już dodane. Jeżeli produktu nie ma już na liście, oznacza to,
-          że został zamówiony.
+          Zajmie Ci to dosłownie chwilę, a dzięki temu nic nie umknie. Twoje
+          zgłoszenie trafi tam, gdzie trzeba i będzie czekało na realizację.
+        </p>
+
+        <p>
+          <strong>📋 Aktualna lista zakupowa</strong> poniżej pokazuje wszystkie
+          produkty oczekujące na zamówienie. Jeżeli produktu nie ma już na
+          liście, oznacza to, że został zamówiony.
+        </p>
+
+        <p className="intro-footer">
+          💙 Dziękujemy za pomoc w utrzymaniu naszego biura!
         </p>
       </div>
     </>
