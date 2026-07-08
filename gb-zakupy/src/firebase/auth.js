50deb310e50c68b8167a763cc37fee51f4486c74
jenny14white <lilianaszymikowska@wp.pl>
2026-07-08 22:26:25 +0200

    Update auth.js

--- gb-zakupy/src/firebase/auth.js
+++ gb-zakupy/src/firebase/auth.js
@@ -1,18 +1,1 @@
-import {
-  signInWithEmailAndPassword,
-  signOut,
-} from 'firebase/auth';
-import { auth } from './firebase';
-import { ADMIN_EMAIL } from '../utils/constants';
 
-export async function loginAdmin(email, password) {
-  if (email.trim().toLowerCase() !== ADMIN_EMAIL?.toLowerCase()) {
-    throw new Error('Brak uprawnień administratora.');
-  }
-
-  return signInWithEmailAndPassword(auth, email, password);
-}
-
-export async function logoutAdmin() {
-  return signOut(auth);
-}
