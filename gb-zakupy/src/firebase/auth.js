496a0cf0eb627a19568982b52a4989ed7add7055
jenny14white <lilianaszymikowska@wp.pl>
2026-07-09 09:01:10 +0200

    Update auth.js

--- gb-zakupy/src/firebase/auth.js
+++ gb-zakupy/src/firebase/auth.js
@@ -1,1 +1,19 @@
+import {
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { auth } from './firebase';
 
+export async function loginAdmin(email, password) {
+  const credential = await signInWithEmailAndPassword(
+    auth,
+    email.trim(),
+    password
+  );
+
+  return credential.user;
+}
+
+export async function logoutAdmin() {
+  await signOut(auth);
+}
