5ca877bec193c29d1514f3200f9df78c86748e99
jenny14white <lilianaszymikowska@wp.pl>
2026-07-16 14:56:51 +0200

    Update Calendar.jsx

--- gb-zakupy/src/components/calendar/Calendar.jsx
+++ gb-zakupy/src/components/calendar/Calendar.jsx
@@ -1,1 +1,61 @@
+import { useMemo, useState } from 'react';
+import CalendarHeader from './CalendarHeader';
 
+export default function Calendar() {
+
+  const [currentDate, setCurrentDate] = useState(
+    new Date()
+  );
+
+  function previousMonth() {
+
+    setCurrentDate((date) =>
+      new Date(
+        date.getFullYear(),
+        date.getMonth() - 1,
+        1
+      )
+    );
+
+  }
+
+  function nextMonth() {
+
+    setCurrentDate((date) =>
+      new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        1
+      )
+    );
+
+  }
+
+  function today() {
+
+    setCurrentDate(new Date());
+
+  }
+
+  return (
+    <>
+
+      <CalendarHeader
+        currentDate={currentDate}
+        onPrev={previousMonth}
+        onNext={nextMonth}
+        onToday={today}
+      />
+
+      <div className="calendar-coming">
+
+        🚧
+
+        Widok kalendarza powstaje...
+
+      </div>
+
+    </>
+  );
+
+}
