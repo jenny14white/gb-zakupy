249bf4a285b50979ebfd6d0ae845563f0f4ca129
jenny14white <lilianaszymikowska@wp.pl>
2026-07-16 14:56:24 +0200

    Update CalendarDay.jsx

--- gb-zakupy/src/components/calendar/CalendarDay.jsx
+++ gb-zakupy/src/components/calendar/CalendarDay.jsx
@@ -1,1 +1,36 @@
 
+export default function CalendarDay({
+  day,
+  isCurrentMonth,
+  isToday,
+  events,
+  onClick,
+}) {
+  return (
+    <div
+      className={`calendar-day
+        ${isCurrentMonth ? '' : 'other-month'}
+        ${isToday ? 'today' : ''}
+      `}
+      onClick={() => onClick(day)}
+    >
+      <div className="calendar-day-number">
+        {day.getDate()}
+      </div>
+
+      <div className="calendar-icons">
+        {events.slice(0, 3).map((event, index) => (
+          <span key={index}>
+            {event.emoji}
+          </span>
+        ))}
+
+        {events.length > 3 && (
+          <small>
+            +{events.length - 3}
+          </small>
+        )}
+      </div>
+    </div>
+  );
+}
