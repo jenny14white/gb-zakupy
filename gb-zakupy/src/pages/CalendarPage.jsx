ff6801414c52fdbffce8823d21de966cd8848ea8
jenny14white <lilianaszymikowska@wp.pl>
2026-07-16 14:57:10 +0200

    Update CalendarPage.jsx

--- gb-zakupy/src/pages/CalendarPage.jsx
+++ gb-zakupy/src/pages/CalendarPage.jsx
@@ -1,1 +1,11 @@
+import Calendar from '../components/calendar/Calendar';
 
+export default function CalendarPage() {
+  return (
+    <main className="calendar-page">
+
+      <Calendar />
+
+    </main>
+  );
+}
