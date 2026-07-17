c6bfba7570cb41a0592fed0eb0e37edf7d5939a8
jenny14white <lilianaszymikowska@wp.pl>
2026-07-17 12:19:36 +0200

    Update main.jsx

--- gb-zakupy/src/main.jsx
+++ gb-zakupy/src/main.jsx
@@ -5,7 +5,10 @@ import App from './App.jsx';
 import './styles/global.css';
 import './styles/publicPage.css';
 import './styles/admin.css';
+
 import './styles/calendar.css';
+import './styles/calendar-desktop.css';
+import './styles/calendar-mobile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
