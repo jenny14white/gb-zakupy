4a3d729aed08eb3c355b92389f9eaffb494af8e3
jenny14white <lilianaszymikowska@wp.pl>
2026-07-17 12:56:07 +0200

    Update accessService.js

--- gb-zakupy/src/services/accessService.js
+++ gb-zakupy/src/services/accessService.js
@@ -1,1 +1,16 @@
+import { COMPANY_ACCESS_CODE } from "../config/accessConfig";
 
+
+export function checkAccessCode(inputCode) {
+
+  if (!inputCode) {
+    return false;
+  }
+
+
+  return (
+    inputCode.trim().toUpperCase() ===
+    COMPANY_ACCESS_CODE.toUpperCase()
+  );
+
+}
