3219e853ee76bbb4222ca2763888559db843d7b4
jenny14white <lilianaszymikowska@wp.pl>
2026-07-21 12:35:53 +0200

    Update GooeyCard.jsx

--- gb-zakupy/src/components/shared/effects/GooeyCard.jsx
+++ gb-zakupy/src/components/shared/effects/GooeyCard.jsx
@@ -1,1 +1,48 @@
+export default function GooeyCard({
+    icon,
+    title,
+    description,
+    onClick
+}) {
 
+
+return (
+
+<button
+className="gooey-card"
+onClick={onClick}
+>
+
+
+<div className="gooey-blob blob-one"></div>
+<div className="gooey-blob blob-two"></div>
+
+
+<div className="gooey-content">
+
+
+<div className="gooey-icon">
+{icon}
+</div>
+
+
+
+<h3>
+{title}
+</h3>
+
+
+<p>
+{description}
+</p>
+
+
+</div>
+
+
+</button>
+
+);
+
+
+}
