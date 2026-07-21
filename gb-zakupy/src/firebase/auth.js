e6621bdbff9ed48ce07352031339f8aaf940fb9f
jenny14white <lilianaszymikowska@wp.pl>
2026-07-21 10:01:36 +0200

    Update auth.js

--- gb-zakupy/src/firebase/auth.js
+++ gb-zakupy/src/firebase/auth.js
@@ -1,10 +1,15 @@
 import {
   signInWithEmailAndPassword,
+  signInAnonymously,
   signOut,
 } from 'firebase/auth';
+
 import { auth } from './firebase';
 
+
+
 export async function loginAdmin(email, password) {
+
   const credential = await signInWithEmailAndPassword(
     auth,
     email.trim(),
@@ -12,8 +17,37 @@ export async function loginAdmin(email, password) {
   );
 
   return credential.user;
+
 }
 
+
+
+
+
+export async function loginPortal() {
+
+  const credential = await signInAnonymously(auth);
+
+  return credential.user;
+
+}
+
+
+
+
+
 export async function logoutAdmin() {
+
   await signOut(auth);
+
+}
+
+
+
+
+
+export async function logoutPortal() {
+
+  await signOut(auth);
+
 }
