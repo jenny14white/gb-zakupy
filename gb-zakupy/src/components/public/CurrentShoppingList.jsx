99165a85dbe2920dbbad59a9cf31a43b64feb94b
jenny14white <lilianaszymikowska@wp.pl>
2026-07-21 11:21:31 +0200

    Update CurrentShoppingList.jsx

--- gb-zakupy/src/components/public/CurrentShoppingList.jsx
+++ gb-zakupy/src/components/public/CurrentShoppingList.jsx
@@ -1,24 +1,136 @@
 import EmptyState from '../shared/EmptyState';
 import ShoppingListItem from './ShoppingListItem';
 
-export default function CurrentShoppingList({ items, loading }) {
-  return (
-    <section className="current-list-wrapper">
-      <h2>Aktualna lista zakupowa</h2>
+import ScrollStack, {
+  ScrollStackItem
+} from '../shared/effects/ScrollStack';
 
-      {loading && <EmptyState>Ładowanie listy...</EmptyState>}
+
+
+
+
+export default function CurrentShoppingList({
+  items,
+  loading
+}) {
+
+
+  return (
+
+
+    <section className="current-list-wrapper">
+
+
+      <h2>
+        Aktualna lista zakupowa
+      </h2>
+
+
+
+
+
+      {loading && (
+
+        <EmptyState>
+          Ładowanie listy...
+        </EmptyState>
+
+      )}
+
+
+
+
+
+
 
       {!loading && items.length === 0 && (
-        <EmptyState>Aktualna lista zakupowa jest pusta.</EmptyState>
+
+        <EmptyState>
+          Aktualna lista zakupowa jest pusta.
+        </EmptyState>
+
       )}
 
+
+
+
+
+
+
       {!loading && items.length > 0 && (
-        <div className="current-list">
-          {items.map((item) => (
-            <ShoppingListItem key={item.id} item={item} />
-          ))}
-        </div>
+
+
+        <ScrollStack
+
+
+          itemDistance={60}
+
+
+          itemStackDistance={35}
+
+
+          stackPosition="20%"
+
+
+          baseScale={0.92}
+
+
+          itemScale={0.02}
+
+
+          rotationAmount={0}
+
+
+          blurAmount={0}
+
+
+          useWindowScroll={true}
+
+
+        >
+
+
+
+          {
+            items.map((item)=>(
+
+
+              <ScrollStackItem
+
+                key={item.id}
+
+              >
+
+
+
+                <ShoppingListItem
+
+                  item={item}
+
+                />
+
+
+
+              </ScrollStackItem>
+
+
+
+            ))
+          }
+
+
+
+
+        </ScrollStack>
+
+
       )}
+
+
+
     </section>
+
+
   );
+
 }
