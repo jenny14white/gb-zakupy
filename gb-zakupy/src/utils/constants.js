a85a668b4c5f0e50c754fc478e4822a5b15579dc
jenny14white <lilianaszymikowska@wp.pl>
2026-07-22 12:23:20 +0200

    Update constants.js

--- gb-zakupy/src/utils/constants.js
+++ gb-zakupy/src/utils/constants.js
@@ -3,14 +3,12 @@ export const ADMIN_EMAIL = import.meta.env.VITE_ADMIN_EMAIL;
 export const ORDER_STATUS = {
   PENDING: 'pending',
   ACCEPTED: 'accepted',
-  ORDERED: 'ordered',
   COMPLETED: 'completed',
 };
 
 export const ORDER_STATUS_LABELS = {
   pending: '🟡 Oczekujące',
   accepted: '🟢 Przyjęte do realizacji',
-  ordered: '🔵 Zamówione',
   completed: '🟣 Zrealizowane',
 };
 
