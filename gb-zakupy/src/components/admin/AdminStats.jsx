b682569ee877c2b72308fb8e7a6e88c4b05f8f40
jenny14white <lilianaszymikowska@wp.pl>
2026-07-22 12:27:57 +0200

    Update AdminStats.jsx

--- gb-zakupy/src/components/admin/AdminStats.jsx
+++ gb-zakupy/src/components/admin/AdminStats.jsx
@@ -2,34 +2,28 @@ export default function AdminStats({
   allCount,
   pendingCount,
   acceptedCount,
-  orderedCount,
   completedCount,
 }) {
   return (
     <div className="stats">
       <div>
         <strong>{allCount}</strong>
-        <span>Wszystkie zgłoszenia</span>
+        <span>📋 Wszystkie zgłoszenia</span>
       </div>
 
       <div>
         <strong>{pendingCount}</strong>
-        <span>Oczekujące</span>
+        <span>🟡 Oczekujące</span>
       </div>
 
       <div>
         <strong>{acceptedCount}</strong>
-        <span>Przyjęte do realizacji</span>
-      </div>
-
-      <div>
-        <strong>{orderedCount}</strong>
-        <span>Zamówione</span>
+        <span>🟢 Przyjęte do realizacji</span>
       </div>
 
       <div>
         <strong>{completedCount}</strong>
-        <span>Zrealizowane</span>
+        <span>🟣 Zrealizowane</span>
       </div>
     </div>
   );
