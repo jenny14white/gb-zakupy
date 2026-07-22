6c4c6ff36f7e6f297401d73d9d41f10e5adb1282
jenny14white <lilianaszymikowska@wp.pl>
2026-07-22 08:24:16 +0200

    Update HomePage.jsx

--- gb-zakupy/src/pages/HomePage.jsx
+++ gb-zakupy/src/pages/HomePage.jsx
@@ -1,8 +1,7 @@
 import logoGB from "../assets/logo.png";
 
-import Ferrofluid from "../components/shared/effects/Ferrofluid";
-
 import GooeyCard from "../components/shared/effects/GooeyCard";
+
 
 export default function HomePage({
 
@@ -23,67 +22,9 @@ return (
 
 
 
-<div className="home-background">
-
-
-<Ferrofluid
-
-colors={[
-
-"#0353a4",
-
-"#023e7d",
-
-"#002855",
-
-"#001845",
-
-"#33415c"
-
-]}
-
-
-
-speed={0.18}
-
-scale={1.5}
-
-turbulence={0.8}
-
-fluidity={0.12}
-
-rimWidth={0.25}
-
-sharpness={2}
-
-shimmer={1.5}
-
-glow={2.2}
-
-flowDirection="down"
-
-opacity={0.75}
-
-mouseInteraction={true}
-
-mouseStrength={0.8}
-
-mouseRadius={0.3}
-
-
-/>
-
-
-</div>
-
-
-
-
-
-
-
 
 <section className="home-container">
+
 
 
 
@@ -130,11 +71,13 @@ GB Sp. z o.o.
 
 
 
+
 <p>
 
 Portal wewnętrzny firmy
 
 </p>
+
 
 
 
@@ -156,17 +99,23 @@ Portal wewnętrzny firmy
 
 
 
+
+
 <GooeyCard
 
-icon=""
 
 title="Lista zakupowa"
 
+
 description="Obsługa zamówień na artykuły biurowe i wyposażenie"
+
 
 onClick={goToShopping}
 
+
 />
+
+
 
 
 
@@ -177,13 +126,15 @@ onClick={goToShopping}
 
 <GooeyCard
 
-icon=""
 
 title="Kalendarz GB"
 
+
 description="Święta, wydarzenia i spotkania firmowe"
 
+
 onClick={goToCalendar}
+
 
 />
 
@@ -194,15 +145,19 @@ onClick={goToCalendar}
 
 
 
+
+
 <GooeyCard
 
-icon=""
 
 title="Panel admina"
 
+
 description="Zarządzanie zamówieniami i wydarzeniami"
 
+
 onClick={goToAdmin}
+
 
 />
 
@@ -212,6 +167,8 @@ onClick={goToAdmin}
 
 
 </div>
+
+
 
 
 
