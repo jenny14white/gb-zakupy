d435c1bd422bb99a019a8c0b0b381cdac85ec18a
jenny14white <lilianaszymikowska@wp.pl>
2026-07-22 11:27:12 +0200

    Update CalendarPage.jsx

--- gb-zakupy/src/pages/CalendarPage.jsx
+++ gb-zakupy/src/pages/CalendarPage.jsx
@@ -2,124 +2,55 @@ import { useState } from "react";
 
 import Calendar from "../components/calendar/Calendar";
 
-
 export default function CalendarPage({
   goBack,
 }) {
-
 
   const [year] = useState(
     new Date().getFullYear()
   );
 
-
-
   return (
-
 
     <main className="calendar-page">
 
-
-
-
-
       <header className="calendar-page-header">
 
-
-
-
-
         <button
-
           type="button"
-
           className="back-button"
-
           onClick={goBack}
-
         >
-
           ← Menu główne
-
         </button>
 
-
-
-
-
-
-
-
-        <div className="calendar-title-box">
-
-
+        <div className="calendar-header-top">
 
           <p className="calendar-eyebrow">
-
             GB Portal
-
           </p>
-
-
-
-
 
           <h1>
-
             Kalendarz firmowy
-
           </h1>
 
-
-
-
-
           <p className="calendar-description">
-
             Święta, wydarzenia oraz spotkania firmowe.
-
           </p>
-
-
-
-
 
         </div>
 
-
-
-
-
-
       </header>
-
-
-
-
-
-
-
 
       <section className="calendar-layout">
 
-
         <Calendar
-
           year={year}
-
         />
-
 
       </section>
 
-
-
-
-
-
-
     </main>
-
 
   );
 
