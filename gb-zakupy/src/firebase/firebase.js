09499b0257ba701db8e68af53db49685d6be09c0
jenny14white <lilianaszymikowska@wp.pl>
2026-07-24 13:24:07 +0200

    Update firebase.js

--- gb-zakupy/src/firebase/firebase.js
+++ gb-zakupy/src/firebase/firebase.js
@@ -1,75 +1,51 @@
-import { initializeApp, getApps } from 'firebase/app';
+import {
+    initializeApp,
+    getApps,
+} from "firebase/app";
 
 import {
-  getAuth
-} from 'firebase/auth';
+    getAuth,
+} from "firebase/auth";
 
 import {
-  getFirestore
-} from 'firebase/firestore';
-
-
-
+    getFirestore,
+} from "firebase/firestore";
 
 
 const firebaseConfig = {
 
+    apiKey:
+        import.meta.env.VITE_FIREBASE_API_KEY,
 
-  apiKey:
-    import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain:
+        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 
+    projectId:
+        import.meta.env.VITE_FIREBASE_PROJECT_ID,
 
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    storageBucket:
+        import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 
+    messagingSenderId:
+        import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
 
-  projectId:
-    import.meta.env.VITE_FIREBASE_PROJECT_ID,
-
-
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-
-
-  messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-
-
-  appId:
-    import.meta.env.VITE_FIREBASE_APP_ID,
-
+    appId:
+        import.meta.env.VITE_FIREBASE_APP_ID,
 
 };
 
 
 
-
-
-
-
-
-
 const app =
-
-  getApps().length
-
-    ?
-
-    getApps()[0]
-
-    :
-
-    initializeApp(firebaseConfig);
+    getApps().length
+        ? getApps()[0]
+        : initializeApp(firebaseConfig);
 
 
 
+export const auth =
+    getAuth(app);
 
 
-
-
-
-export const auth = getAuth(app);
-
-
-
-export const db = getFirestore(app);
+export const db =
+    getFirestore(app);
