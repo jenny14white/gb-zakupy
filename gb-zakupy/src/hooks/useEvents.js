cd3c9f3e130af7f47c825b322ebfa1f2549591ca
jenny14white <lilianaszymikowska@wp.pl>
2026-07-24 12:43:59 +0200

    Update useEvents.js

--- gb-zakupy/src/hooks/useEvents.js
+++ gb-zakupy/src/hooks/useEvents.js
@@ -1,23 +1,48 @@
 import { useEffect, useState } from "react";
 import { listenToEvents } from "../services/calendarService";
 
-export function useEvents() {
+
+export function useEvents(enabled = true) {
+
     const [events, setEvents] = useState([]);
     const [loading, setLoading] = useState(true);
 
+
     useEffect(() => {
+
+        if (!enabled) {
+
+            setEvents([]);
+            setLoading(false);
+
+            return;
+
+        }
+
+
+        setLoading(true);
+
+
         const unsubscribe = listenToEvents((data) => {
+
             setEvents(data);
             setLoading(false);
+
         });
 
+
         return unsubscribe;
-    }, []);
+
+
+    }, [enabled]);
+
 
     return {
         events,
         loading,
     };
+
 }
+
 
 export default useEvents;
