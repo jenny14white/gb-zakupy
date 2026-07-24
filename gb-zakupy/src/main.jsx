66d05cbc2628a4fcfb536bc13b9f8134762cb9a5
jenny14white <lilianaszymikowska@wp.pl>
2026-07-24 12:49:15 +0200

    Update main.jsx

--- gb-zakupy/src/main.jsx
+++ gb-zakupy/src/main.jsx
@@ -5,16 +5,8 @@ import "./i18n";
 
 import App from "./App.jsx";
 
-/* ===============================
-   CONTEXT
-================================ */
-
 import { ThemeProvider } from "./context/ThemeContext";
 import { LanguageProvider } from "./context/LanguageContext";
-
-/* ===============================
-   GLOBALNE STYLE
-================================ */
 
 import "./styles/global.css";
 import "./styles/theme.css";
@@ -23,29 +15,22 @@ import "./styles/switchers.css";
 import "./styles/publicPage.css";
 import "./styles/admin.css";
 
-/* ===============================
-   KALENDARZ
-================================ */
-
 import "./styles/calendar.css";
 import "./styles/calendar-desktop.css";
 import "./styles/calendar-mobile.css";
 
-/* ===============================
-   DOSTĘP + MENU
-================================ */
-
 import "./styles/access.css";
 import "./styles/home.css";
 
+
 ReactDOM.createRoot(
-  document.getElementById("root")
+    document.getElementById("root")
 ).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider>
+            <LanguageProvider>
+                <App />
+            </LanguageProvider>
+        </ThemeProvider>
+    </React.StrictMode>
 );
