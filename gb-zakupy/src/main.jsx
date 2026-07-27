3fc19ed8cd132d9f50599d0a5da8391234a22f74
jenny14white <lilianaszymikowska@wp.pl>
2026-07-27 10:25:17 +0200

    Update main.jsx

--- gb-zakupy/src/main.jsx
+++ gb-zakupy/src/main.jsx
@@ -8,29 +8,55 @@ import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext";
 import { LanguageProvider } from "./context/LanguageContext";
 
-import "./styles/global.css";
-import "./styles/theme.css";
-import "./styles/switchers.css";
 
-import "./styles/publicPage.css";
+/*
+====================================================
+ GLOBAL
+====================================================
+*/
+
+import "./styles/theme.css";
+import "./styles/global.css";
+
+
+/*
+====================================================
+ COMPONENTS
+====================================================
+*/
+
 import "./styles/admin.css";
 
 import "./styles/calendar.css";
 import "./styles/calendar-desktop.css";
 import "./styles/calendar-mobile.css";
 
-import "./styles/access.css";
+import "./styles/publicPage.css";
+
 import "./styles/home.css";
+
+import "./styles/access.css";
+
+import "./styles/switchers.css";
+
 
 
 ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
+
     <React.StrictMode>
+
         <ThemeProvider>
+
             <LanguageProvider>
+
                 <App />
+
             </LanguageProvider>
+
         </ThemeProvider>
+
     </React.StrictMode>
+
 );
