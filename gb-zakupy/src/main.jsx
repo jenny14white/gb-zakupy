e2216a89f942eb472b1891cf4d4de89afe1db4c5
jenny14white <lilianaszymikowska@wp.pl>
2026-07-28 09:58:33 +0200

    Update main.jsx

--- gb-zakupy/src/main.jsx
+++ gb-zakupy/src/main.jsx
@@ -11,6 +11,11 @@ import {LanguageProvider} from "./context/LanguageContext";
 import "./styles/theme.css";
 import "./styles/global.css";
 
+import "./styles/home.css";
+import "./styles/publicPage.css";
+import "./styles/access.css";
+import "./styles/switchers.css";
+
 import "./styles/admin-theme.css";
 import "./styles/admin.css";
 import "./styles/admin-dashboard.css";
@@ -19,12 +24,6 @@ import "./styles/admin-calendar.css";
 import "./styles/calendar.css";
 import "./styles/calendar-desktop.css";
 import "./styles/calendar-mobile.css";
-
-import "./styles/publicPage.css";
-import "./styles/home.css";
-import "./styles/access.css";
-import "./styles/switchers.css";
-
 
 ReactDOM.createRoot(
     document.getElementById("root")
